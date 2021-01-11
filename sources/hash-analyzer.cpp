535fc86acb8ca2b99ffea0521973bc0d01675fa9
gnole <olegbouchin@gmail.com>
2021-01-12 02:55:55 +0300

    fix

--- sources/hash-analyzer.cpp
+++ sources/hash-analyzer.cpp
@@ -17,7 +17,7 @@ void to_json(json& json_, const Data& jData) {
            {"data", jData.argument}};
 }
 
-[[noreturn]] void HashResearcher::one_process() {
+ void HashResearcher::one_process() {
   while (true) {
     const std::string random_data = std::to_string(rand());
     const std::string hash =
