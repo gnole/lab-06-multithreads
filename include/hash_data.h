1fbdde8fa9865bd9a2431ba568a5350daf2bf87e
gnole <olegbouchin@gmail.com>
2021-01-12 01:56:14 +0300

    hash data h

---
+++ include/hash_data.h
@@ -0,0 +1,18 @@
+// Created by perfest on 12/26/20.
+#ifndef HASH_ANALYZER_PRJ_HASH_DATA_H
+#define HASH_ANALYZER_PRJ_HASH_DATA_H
+
+#include <iostream>
+
+struct Data {
+  std::string hash;
+  std::string argument;
+  std::size_t timeStamp;
+  Data(const std::string& s1,
+       const std::string& s2,
+       const std::size_t& thId) : hash(s1),
+                           argument(s2),
+                           timeStamp(thId) {};
+};
+
+#endif  // HASH_ANALYZER_PRJ_HASH_DATA_H
